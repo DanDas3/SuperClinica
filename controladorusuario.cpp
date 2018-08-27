b63420c5921cd02e856d7342c14f4be82eb2db77
Danilo Araújo <daniloaraujo1990@hotmail.com>
2018-08-27 01:34:16 -0300

    Correção no singleton
    Correção na implementação do singleton do controlador do usuário

--- controladorusuario.cpp
+++ controladorusuario.cpp
@@ -1,14 +1,22 @@
 #include "controladorusuario.h"
 
+ControladorUsuario* ControladorUsuario::instance = 0;
+
 ControladorUsuario::ControladorUsuario()
 {
 
 }
 
-/*ControladorUsuario *ControladorUsuario::getInstance()
+ControladorUsuario::~ControladorUsuario()
+{
+
+}
+
+ControladorUsuario *ControladorUsuario::getInstance()
 {
     if(instance == 0) {
         instance = new ControladorUsuario();
+        return instance;
     }
     return instance;
-}*/
+}
--- controladorusuario.h
+++ controladorusuario.h
@@ -1,16 +1,21 @@
 #ifndef CONTROLADORUSUARIO_H
 #define CONTROLADORUSUARIO_H
 
+#include "repositoriousuario.h"
+
+
 
 class ControladorUsuario
 {
 public:
 
-    //static ControladorUsuario *getInstance();
+    static ControladorUsuario *getInstance();
 
 private:
     ControladorUsuario();
-   // static ControladorUsuario *instance;
+    static ControladorUsuario *instance;
+    RepositorioUsuario repositorio;
+    virtual ~ControladorUsuario();
 };
 
 #endif // CONTROLADORUSUARIO_H
